635c174bd23ecc642c8415952016d64114397067
AnikeeW <egor4ik048@gmail.com>
2024-02-13 19:33:33 +0300

    fix bug

--- src/components/track/sceletonTracksPage/SceletonTracksPage.tsx
+++ src/components/track/sceletonTracksPage/SceletonTracksPage.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import styles from './SceletonTracksPage.module.scss';
 
 const SceletonTracksPage = () => {
-  const arr = [1, 1, 1, 11, 1, 1, 11, 1, 1, 1, 1];
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   console.log(arr);
   return (
     <div className={styles.root}>
       <div className={styles.root__title}>Мои треки</div>
       <div className={styles.root__list}>
         {arr.map((track) => (
-          <div className={styles.root__item}>
+          <div key={track} className={styles.root__item}>
             <div className={styles.root__image}></div>
             <div className={styles.root__info}>
               <div className={styles.root__trackName}></div>
