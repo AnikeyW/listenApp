30f13dd26e3212f766529320002efa4c61fa22fc
AnikeeW <egor4ik048@gmail.com>
2024-02-20 17:52:45 +0300

    fix

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -3,6 +3,7 @@ import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authConfig: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
