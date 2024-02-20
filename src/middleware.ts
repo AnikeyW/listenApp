e0a7e415359b5b11f26008fa70bd8d125a475005
AnikeeW <egor4ik048@gmail.com>
2024-02-20 18:03:17 +0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/create', '/protected/:path*'] };
+export const config = { matcher: ['/create', '/create/:path*'] };
