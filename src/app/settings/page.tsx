3eda1ba4eb802aac763e48b93b318e8eab3e1e85
AnikeeW <egor4ik048@gmail.com>
2024-02-25 20:45:55 +0300

    remove useSession

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,18 +2,15 @@
 import React from 'react';
 import styles from './page.module.scss';
 import ThemeToggle from '@/components/UI/themeToggle/ThemeToggle';
-import { useSession, signOut } from 'next-auth/react';
 import Button from '@/components/UI/Button/Button';
 import { useRouter } from 'next/navigation';
 import { useAuthStore } from '@/stores/authStore';
-import authService from '@/services/Auth.service';
 import { useLogout } from '@/hooks/useLogout';
 import ErrorMessage from '@/components/UI/ErrorMessage/ErrorMessage';
 import $api from '@/http';
 
 const Page = () => {
   const router = useRouter();
-  const session = useSession();
   const isAuth = useAuthStore((state) => state.isAuth);
   const user = useAuthStore((state) => state.user);
   const logout = useLogout();
@@ -31,23 +28,21 @@ const Page = () => {
 
   return (
     <div className={styles.root}>
-      {session.data || isAuth ? (
+      {isAuth ? (
         <>
-          {session.data?.user?.image ||
-            (user?.image && (
-              <img
-                src={session.data?.user?.image || user.image}
-                alt={'profile photo'}
-                width={50}
-                height={50}
-              />
-            ))}
+          {user?.image && (
+            <img
+              src={user.image}
+              alt={'profile photo'}
+              width={50}
+              height={50}
+            />
+          )}
 
-          <div>{session.data?.user?.name || user?.name}</div>
+          <div>{user?.name}</div>
           <div>
             <Button
               onClick={() => {
-                // signOut({ callbackUrl: '/' });
                 logout.mutate();
               }}
             >
@@ -57,7 +52,7 @@ const Page = () => {
           {logout.isError && <ErrorMessage message={logout.error.message} />}
         </>
       ) : (
-        <Button onClick={() => router.replace('/signin')}>Войти</Button>
+        <Button onClick={() => router.push('/signin')}>Войти</Button>
       )}
       <ThemeToggle />
       <Button onClick={() => getProfile()}>Тест</Button>
--- src/components/mainSection/mainSection.tsx
+++ src/components/mainSection/mainSection.tsx
@@ -1,24 +1,15 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './MainSection.module.scss';
 import { usePlayerStore } from '@/stores/playerStore';
-import { useSession } from 'next-auth/react';
-import { useAuthStore } from '@/stores/authStore';
 
 interface IMainSectionProps {
   children: React.ReactNode;
 }
 
 const MainSection: React.FC<IMainSectionProps> = ({ children }) => {
-  // const session = useSession();
   const activeTrack = usePlayerStore((state) => state.activeTrack);
-  const setUser = useAuthStore((state) => state.setUser);
 
-  // useEffect(() => {
-  //   if (session.data?.user) {
-  //     setUser(session.data.user);
-  //   }
-  // });
   return (
     <main
       className={`${styles.main} ${activeTrack ? styles.withActiveTrack : ''}`}
