a535213e77c0bf11abbeb65a02dc5d9e8ddd6d80
AnikeeW <egor4ik048@gmail.com>
2024-02-25 14:18:29 +0300

    add response interceptor

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { IAuthLoginResponse } from '@/types/auth';
 
 const $api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -14,5 +15,30 @@ $api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.get<IAuthLoginResponse>(
+          process.env.NEXT_PUBLIC_BASE_URL + 'auth/refresh',
+          {
+            withCredentials: true,
+          },
+        );
+        localStorage.setItem('accessToken', response.data.accessToken);
+        $api.request(originalRequest);
+      } catch (e) {
+        console.log('Не авторизован');
+      }
+    }
+    throw error;
+  },
+);
 
 export default $api;
