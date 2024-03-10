82206b6dc5a76f6b690b63c9d8e075326ad55d41
AnikeeW <egor4ik048@gmail.com>
2024-03-10 17:18:06 +0300

    change IUser

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,12 @@
+interface IFavoriteTrack {
+  trackId: string;
+  addedAt: string;
+}
+
 export interface IUser {
   _id: string;
   image: string;
   email: string;
   name: string;
-  favoritesTracks: string[];
+  favoritesTracks: IFavoriteTrack[];
 }
